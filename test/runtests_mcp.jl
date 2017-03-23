5ee21686e183c074ed564daee0bb07110148be7d
Changhyun Kwon <chkwon@usf.edu>
2017-03-23 14:59:38 -0400

    test files updated

--- test/runtests_mcp.jl
+++ test/runtests_mcp.jl
@@ -1,9 +1,7 @@
 using Complementarity
 using Base.Test
 
-include("../examples/mcp/transmcp.jl")
-include("hhoeschle_mcp1.jl")
-
+info("-------[Testing Complementarity/PATHSolver]------------------------------------------")
 
 #########################################################################
 m = MCPModel()
@@ -151,79 +149,3 @@ z = getvalue(x)
 
 
 println("------------------------------------------------------------------")
-
-println("-------[Testing NLSolve]------------------------------------------")
-
-
-#########################################################################
-m = nothing
-m = MCPModel()
-
-M = [0  0 -1 -1 ;
-     0  0  1 -2 ;
-     1 -1  2 -2 ;
-     1  2 -2  4 ]
-
-q = [2; 2; -2; -6]
-
-lb = zeros(4)
-ub = Inf*ones(4)
-
-items = 1:4
-
-# @variable(m, lb[i] <= x[i in items] <= ub[i])
-@variable(m, x[i in items] >= 0)
-@mapping(m, F[i in items], sum(M[i,j]*x[j] for j in items) + q[i])
-@complementarity(m, F, x)
-
-status = solveMCP(m, solver=:NLsolve)
-@show status
-
-z = getvalue(x)
-# Fz = getvalue(F)
-
-@show z
-# @show Fz
-
-# @test isapprox(z[1], 2.8)
-# @test isapprox(z[2], 0.0)
-# @test isapprox(z[3], 0.8)
-# @test isapprox(z[4], 1.2)
-#########################################################################
-
-println("------------------------------------------------------------------")
-
-#########################################################################
-m = nothing
-m = MCPModel()
-
-lb = zeros(4)
-ub = Inf*ones(4)
-items = 1:4
-@variable(m, lb[i] <= x[i in items] <= ub[i])
-
-@mapping(m, F1, 3*x[1]^2+2*x[1]*x[2]+2*x[2]^2+x[3]+3*x[4]-6)
-@mapping(m, F2, 2*x[1]^2+x[1]+x[2]^2+3*x[3]+2*x[4]-2)
-@mapping(m, F3, 3*x[1]^2+x[1]*x[2]+2*x[2]^2+2*x[3]+3*x[4]-1)
-@mapping(m, F4, x[1]^2+3*x[2]^2+2*x[3]+3*x[4]-3)
-
-@complementarity(m, F1, x[1])
-@complementarity(m, F2, x[2])
-@complementarity(m, F3, x[3])
-@complementarity(m, F4, x[4])
-
-status = solveMCP(m, solver=:NLsolve, method=:trust_region)
-@show status
-
-z = getvalue(x)
-Fz = [getvalue(F1), getvalue(F2), getvalue(F3), getvalue(F4)]
-
-@show z
-@show Fz
-
-# @test isapprox(z[1], 1.2247, atol=1e-4)
-# @test isapprox(z[2], 0.0, atol=1e-4)
-# @test isapprox(z[3], 0.0, atol=1e-4)
-# @test isapprox(z[4], 0.5, atol=1e-4)
-
-#########################################################################
--- test/runtests_mpec.jl
+++ test/runtests_mpec.jl
@@ -1,4 +1,7 @@
 
+
+info("-------[Testing Complementarity/MPEC]------------------------------------------")
+
 include("../examples/mpec/bard1.jl")
 include("../examples/mpec/dempe.jl")
 include("../examples/mpec/gnash1m.jl")
